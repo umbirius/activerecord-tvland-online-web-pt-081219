bd8643abe93ad9960fdcb25876edf0bad3eda61d
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-10 18:49:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters 
   
   def actors_list
-    self.actors.full_name
+    self.actors.map {|actor| actor.full_name}
   end 
 end
